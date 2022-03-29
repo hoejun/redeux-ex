168befb8ad8aceff6bb49b5d9b336daf69dc6ac4
hoejun <hjkim.it@gmail.com>
2022-03-29 12:50:26 +0900

    리덕스

---
+++ src/modules/counter.js
@@ -0,0 +1,23 @@
+import { createAction, handleActions } from 'redux-actions';
+
+// 액션 타입 정의
+const INCREASE = 'counter/INCREASE';
+const DECREASE = 'counter/DECREASE';
+
+// 액션 생성함수
+export const increase = createAction(INCREASE);
+export const decrease = createAction(DECREASE);
+
+const initialState = {
+  number: 0,
+};
+
+const counter = handleActions(
+  {
+    [INCREASE]: (state, action) => ({ number: state.number + 1 }),
+    [DECREASE]: (state, action) => ({ number: state.number - 1 }),
+  },
+  initialState
+);
+
+export default counter;
