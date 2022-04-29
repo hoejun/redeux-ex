3d7813682d14092bbdef42a2e105f713da23f5d5
hoejun <hjkim.it@gmail.com>
2022-04-29 16:08:58 +0900

    리베이스 테스트1

--- src/components/Todos.js
+++ src/components/Todos.js
@@ -24,7 +24,6 @@ const Todos = ({ input, todos, onChangeInput, onInsert, onToggle, onRemove }) =>
 
   return (
     <div>
-      {console.log('테스트 브랜치')}
       <form onSubmit={onSubmit}>
         <div>d</div>
         <input value={input} onChange={onChange} />
